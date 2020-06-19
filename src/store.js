883e16638759056b27d784832c75959fa6f8f91e
Your Name <abhilashpandey8170@gmail.com>
2020-06-19 16:55:40 +0530

    some build thing

--- src/store.js
+++ src/store.js
@@ -3,7 +3,7 @@ import {logger} from 'redux-logger'
 import thunk from 'redux-thunk'
 import Allreducer from './reducers/rootreducers'
 const initialstate={}
-const middleware=[logger,thunk]
+const middleware=[thunk]
 const store=createStore(Allreducer,initialstate,  applyMiddleware(...middleware))
 
 export default store;
